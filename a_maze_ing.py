b03ba569301a861d28d702a0fd2c39c2d27d4cb4
Rodrigo German Llorens <rgerman-@c2r11s6.42madrid.com>
2026-01-09 12:33:12 +0100

    Basic structure for the main classes of the project

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -5,6 +5,7 @@ from MazeConfig import MazeConfig
 class Maze:
     def __init__(self):
         self.config : MazeConfig = MazeConfig()
+        
 
 if __name__ == "__main__":
     maze : Maze = Maze()
---
+++ src/MazeRenderer.py
@@ -0,0 +1,2 @@
+class MazeRenderer:
+    pass
